101ace22dd6eab62e3b2a174a046a7b10700c15a
jenniifer99 <63098327+jenniifer99@users.noreply.github.com>
2021-01-05 14:13:37 +0100

    visa vänförfrågningar, alla funktioner inte klara

---
+++ DataLayer/Repositories/FriendRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Repositories
+{
+    public class FriendRepository : Repository<Friend>
+    {
+        public FriendRepository(DatingAppContext context) : base(context) { }
+
+        public void AddFriend(Friend friend)
+        {
+            items.Add(friend);
+            SaveChanges();
+        }
+    }
+}
---
+++ DataLayer/Repositories/FriendRequestRepository.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer.Repositories
+{
+    public class FriendRequestRepository : Repository<FriendRequest>
+    {
+        public FriendRequestRepository(DatingAppContext context) : base(context) { }
+
+        public FriendRequest GetFriendRequestById(int id)
+        {
+            return items.FirstOrDefault((r) => r.FriendRequestId == id);
+        }
+
+        public List<FriendRequest> GetAllRequestsSentToUser(int id)
+        {
+            return items.Where((r) => r.ReceiverId.Equals(id)).ToList();
+        }
+
+        public void DeleteRequest(int id)
+        {
+            FriendRequest requestToDelete = GetFriendRequestById(id);
+            items.Remove(requestToDelete);
+            SaveChanges();
+        }
+    }
+}
