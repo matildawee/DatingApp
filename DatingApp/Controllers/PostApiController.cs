6b7fa8d52854c7c23e87ad77c94dd6accd7eaf53
matildawe <63040887+matildawe@users.noreply.github.com>
2021-01-05 18:29:25 +0100

    update för addpost

---
+++ DatingApp/Controllers/PostApiController.cs
@@ -0,0 +1,62 @@
+﻿using DataLayer;
+using DataLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+
+namespace DatingApp.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class PostApiController : ApiController
+    {
+        private readonly DatingAppContext _context;
+
+        public PostApiController(DatingAppContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public void AddPost(Post post)
+        {
+            
+            var ja = post.PersonId;
+            var hej = "h";
+            //if (ModelState.IsValid)
+            //{
+            //    string postTo;
+            //    if (string.IsNullOrWhiteSpace(post.PostToId))
+            //    {
+            //        postTo = User.Identity.GetUserId();
+            //    }
+            //    else
+            //    {
+            //        postTo = post.PostToId;
+            //    }
+
+            //    PostModels postModel = new PostModels()
+            //    {
+            //        Text = post.Text,
+            //        PostFromId = User.Identity.GetUserId(),
+            //        PostToId = postTo,
+            //        PostDateTime = DateTime.Now
+            //    };
+
+            //    postRepository.Add(postModel);
+            //    postRepository.Save();
+            //}
+        }
+
+        //[HttpDelete]
+        //public void DeletePost(int id)
+        //{
+        //    postRepository.Remove(id);
+        //    postRepository.Save();
+        //}
+    } 
+}
--- DatingApp/wwwroot/js/post.js
+++ DatingApp/wwwroot/js/post.js
@@ -1,19 +1,65 @@
 ﻿
 
-    $("#SubmitPost").click(function () {
-        //e.preventDefault();
-        $("#TEST").hide();
-                var post = new Post();
-                post.Person = Model.PersonId;
-                post.Author = $(User).val();
-                post.PostText = $('#PostText').val();
-                $.ajax({
-            url: "/PostsApiController/PostPost/",
+//$("#SubmitPost").click(function () {
+//    //e.preventDefault();
+
+//    var post;
+//    var id = 1;
+//    var text = "neeej";
+//    post = { PostText: text, PersonId: id };
+//    //post.Author = 2;
+//    //post.Post = "neej";
+//    //post.Person = Model.PersonId;
+//    //post.Author = $(User).val();
+//    //post.PostText = $('#PostText').val();
+//    $("#TEST").hide();
+//    $.ajax({
+//        type: "POST",
+//        url: "/api/PostsApi/",        
+//        data: JSON.stringify(post),
+//        contentType: "application/json; charset=utf-8",
+//        //dataType: "json",
+//        success: alert("Success!"),
+//        error: () => {
+//            alert("Error: Failed to add new post");
+//        }
+//    });
+//});
+
+$("#PostWall").on("click", "#SubmitPost", AddPost);
+
+function AddPost() {
+    //var currentUrl = window.location.href;
+    //var urlArray = currentUrl.split("/Profile/Index/");
+    //var Id = "";
+    if ($("#PostText").val() != "" && $("#PostText").val().length <= 280) {
+        var post;
+        //if (urlArray.length >= 2) {
+        //    Id = urlArray[1];
+        //}
+        //if (!Id == "") {
+        //    post = { Text: $("#PostText").val(), PostToId: Id };
+        //} else {
+        //    post = { Text: $("#PostText").val() };
+        //}
+        var id = 1;
+        var text = "neeej";
+        post = { PostText: text, PersonId: id };
+        var hej;
+        $.ajax({
             type: "POST",
-            data: post,
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: alert("sent")
+            url: "/PostApi/AddPost",
+            data: JSON.stringify(post),
+            contentType: "application/json;charset=UTF-8",
+            success: () => {
+                //Update_Wall();
+                alert(":O");
+            },
+            error: () => {
+                alert("Error: Failure to add post");
+            }
         });
-    });
-       
+    } else {
+        alert("Your post needs to consist of between 1 and 280 characters.")
+    }
+}
